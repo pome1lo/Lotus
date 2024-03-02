f0f75c43407af55bd7c0208454c85cf5a4a1a0c3
zephyrXXX <alxbraff@mail.ru>
2024-03-02 21:06:04 +0300

    added the ability to change the theme

--- Lotus/UI/client.react/src/components/Header.js
+++ Lotus/UI/client.react/src/components/Header.js
@@ -1,58 +1,23 @@
 import '../resources/css/Header.css';
 import Logo from "../resources/images/logo/big_logo.svg";
+import React, { useState } from 'react';
+
 
 function Header() {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+        } else {
+            setTheme('light');
+            document.documentElement.setAttribute('data-bs-theme', 'light');
+        }
+    };
+
     return (
         <div>
-            <div className="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-                <button className="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme"
-                        type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (light)">
-                    <svg className="bi my-1 theme-icon-active" width="1em" height="1em">
-                        <use href="#sun-fill"></use>
-                    </svg>
-                    <span className="visually-hidden" id="bd-theme-text">Toggle theme</span>
-                </button>
-                <ul className="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-                    <li>
-                        <button type="button" className="dropdown-item d-flex align-items-center active"
-                                data-bs-theme-value="light" aria-pressed="true">
-                            <svg className="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
-                                <use href="#sun-fill"></use>
-                            </svg>
-                            Light
-                            <svg className="bi ms-auto d-none" width="1em" height="1em">
-                                <use href="#check2"></use>
-                            </svg>
-                        </button>
-                    </li>
-                    <li>
-                        <button type="button" className="dropdown-item d-flex align-items-center"
-                                data-bs-theme-value="dark"
-                                aria-pressed="false">
-                            <svg className="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
-                                <use href="#moon-stars-fill"></use>
-                            </svg>
-                            Dark
-                            <svg className="bi ms-auto d-none" width="1em" height="1em">
-                                <use href="#check2"></use>
-                            </svg>
-                        </button>
-                    </li>
-                    <li>
-                        <button type="button" className="dropdown-item d-flex align-items-center"
-                                data-bs-theme-value="auto"
-                                aria-pressed="false">
-                            <svg className="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
-                                <use href="#circle-half"></use>
-                            </svg>
-                            Auto
-                            <svg className="bi ms-auto d-none" width="1em" height="1em">
-                                <use href="#check2"></use>
-                            </svg>
-                        </button>
-                    </li>
-                </ul>
-            </div>
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                     <div className="container-fluid">
@@ -77,6 +42,12 @@ function Header() {
                                 </li>
                             </ul>
                             <form className="d-flex" role="search">
+                                <div>
+                                    <label className="theme-switch">
+                                        <input type="checkbox" onChange={toggleTheme}/>
+                                        <span className="slider"></span>
+                                    </label>
+                                </div>
                                 <input className="form-control me-2" type="search" placeholder="Search"
                                        aria-label="Search"/>
                                 <button className="btn btn-outline-success" type="submit">Search</button>
