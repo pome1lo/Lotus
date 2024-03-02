edc6b205a70f6fe3e0716970e4aa4e84e4f60a84
zephyrXXX <alxbraff@mail.ru>
2024-03-03 01:19:38 +0300

    added integration with AOS Library

--- Lotus/UI/client.react/src/App.js
+++ Lotus/UI/client.react/src/App.js
@@ -4,8 +4,15 @@ import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 import Post from "./components/Post";
 import Recommendations from "./components/Recommendations";
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Импортируйте CSS из библиотеки AOS
 
 function App() {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
   return (
       <div className="App">
           <div className="container-xxl">
--- Lotus/UI/client.react/src/components/Post.js
+++ Lotus/UI/client.react/src/components/Post.js
@@ -12,9 +12,8 @@ function Post() {
             spread: 60
         });
     }
-
     return (
-        <div className="post">
+        <div data-aos="fade-up" data-aos-anchor-placement="center-bottom" className="post" data-aos-duration="750">
         {/*<div className="col-md-6 post">*/}
             <div>
                 <Publisher avatar={Avatar} nickname="Travis Scott" info="31 Jan. 15:23" />
