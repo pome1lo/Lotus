e43cd4de24de8b46392cca2d8471783d5859e068
zephyrXXX <alxbraff@mail.ru>
2024-03-08 20:40:32 +0300

    add Redis caching

--- Lotus/Microservices/authentication.service/src/routes/userAccount.js
+++ Lotus/Microservices/authentication.service/src/routes/userAccount.js
@@ -2,15 +2,19 @@ const Router = require('koa-router');
 const jwt = require('jsonwebtoken');
 const koaJwt = require('koa-jwt');
 const argon2 = require('argon2');
+const redis = require('redis');
+const { promisify } = require('util');
 const { USER: User } = require('../database/models/user');
 
 const router = new Router();
-const secretKey = 'your-secret-key';
+const secretKey = 'your-secret-key'; // docker
 
-router.post('/register', async (ctx) => {
+const client = redis.createClient("redis://localhost:6379");
+client.connect();
+
+router.post('/userAccount/register', async (ctx) => {
     const { username, password } = ctx.request.body;
 
-    // Проверка, существует ли пользователь в базе данных
     const existingUser = await User.findOne({ where: { username } });
     if (existingUser) {
         ctx.status = 400;
@@ -18,31 +22,37 @@ router.post('/register', async (ctx) => {
         return;
     }
 
-    // Если пользователя нет, хешируем пароль и добавляем его в базу данных
     const hashedPassword = await argon2.hash(password);
     const newUser = await User.create({ username, password: hashedPassword });
+
+    await client.set(username, JSON.stringify(newUser));
 
     ctx.status = 201;
     ctx.body = { message: 'Пользователь успешно зарегистрирован', user: newUser.username };
 });
 
-router.post('/auth', async (ctx) => {
+router.post('/userAccount/auth', async (ctx) => {
     const { username, password } = ctx.request.body;
 
+    let user = JSON.parse(await client.get(username));
 
-    const user = await User.findOne({ where: { username } });
+    if (!user) {
+        user = await User.findOne({ where: { username } });
+        if (user) {
+            await client.set(username, JSON.stringify(user));
+        }
+    }
+
     if (!user || !(await argon2.verify(user.password, password))) {
         ctx.status = 401;
         ctx.body = { message: 'Неверное имя пользователя или пароль' };
         return;
     }
 
-
     const token = jwt.sign({ username }, secretKey);
     ctx.body = { token };
 });
-
-router.get('/protected', koaJwt({ secret: secretKey }), async (ctx) => {
+router.get('/userAccount/protected', koaJwt({ secret: secretKey }), async (ctx) => {
     ctx.body = { message: 'Вы успешно прошли аутентификацию!' };
 });
 
