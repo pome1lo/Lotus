e877e31ac99c7f1304d3768b6f52e6d44d2c17d5
zephyrXXX <alxbraff@mail.ru>
2024-03-10 00:31:27 +0300

    add Auth component

---
+++ Lotus/UI/client.react/src/components/Auth.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+function Auth() {
+    const [inputCode, setCode] = useState('');
+    const [timeLeft, setTimeLeft] = useState(10);
+    const [isButtonDisabled, setButtonDisabled] = useState(true);
+
+    useEffect(() => {
+        if (timeLeft > 0) {
+            const timerId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+            return () => clearTimeout(timerId);
+        } else {
+            setButtonDisabled(false);
+        }
+    }, [timeLeft]);
+
+    async function register() {
+        console.log('great!');
+    }
+
+    async function sendAgain() {
+        console.log('great!');
+        setTimeLeft(10);
+        setButtonDisabled(true);
+    }
+
+    return (
+        <form onSubmit={register} className={"text-center form-signin"}>
+            <h2 className="h4 mb-3 fw-medium">Insert the code</h2>
+            <div className={"form-floating"}>
+                <input type="number" className="form-control" id="floatingPassword" placeholder="Code" required
+                       value={inputCode} onChange={(e) => setCode(e.target.value)}/>
+                <label htmlFor="floatingPassword">Code</label>
+            </div>
+
+            <div className="d-flex justify-content-between align-items-center">
+                <button className="btn btn-link rounded-pill rememberMe redHover"  disabled={isButtonDisabled} type="button" onClick={sendAgain}>Send again</button>
+                <p className={"rememberMe"}>{timeLeft > 0 ? `0:${timeLeft} c.` : '0:0 c.'}</p>
+            </div>
+            <button className="btn  btn-danger w-100 py-2" type="submit">Verify</button>
+        </form>
+    );
+}
+
+export default Auth;
