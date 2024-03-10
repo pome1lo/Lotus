0206ff3a0738947f2d73ff0fb784729457a9352d
zephyrXXX <alxbraff@mail.ru>
2024-03-10 17:20:55 +0300

    add profile service | setup RabbitMQ

---
+++ Lotus/Microservices/profile.service/src/database/models/user.js
@@ -0,0 +1,23 @@
+const { DataTypes} = require('sequelize');
+const sequelize = require('../config.js');
+
+const USER = sequelize.define('USERS', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
+
+module.exports = { USER };
